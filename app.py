ff8fc20b8018f1351044a6442fb61691357b925d
Lgreen02 <127134248+Lgreen02@users.noreply.github.com>
2025-07-10 12:19:41 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 import os
+
 app = Flask(__name__)
-CORS(app)  # Allows frontend on a different port to access this backend
+CORS(app)
+
+@app.route('/')
+def index():
+    return 'Welcome to SlideSite Backend!'
 
 @app.route('/api/hello', methods=['GET'])
 def hello():
     return jsonify(message="Hello World")
 
-
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # fallback to 5000 locally
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
